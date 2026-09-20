0f57ad3a0dd45a65e0d5b16bda7905f118733a15
ramalho031@gmail.com <ivanizio@DESKTOP-Q5D87MD.localdomain>
2026-09-20 08:16:55 -0300

    Corrige caminhos do ambiente e banco de dados

--- database/database.js
+++ database/database.js
@@ -1,6 +1,10 @@
 const Database = require("better-sqlite3");
 
-const db = new Database("database/contatos.db");
+const path = require("path");
+
+const caminhoBanco = path.join(__dirname, "contatos.db");
+
+const db = new Database(caminhoBanco);
 
 db.exec(`
     CREATE TABLE IF NOT EXISTS contatos (
