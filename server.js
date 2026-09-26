e5fc17884c6b552eec6cf2f17a046dd448f5ec58
Ivanizio Ramalho <135184419+ivanizio@users.noreply.github.com>
2026-09-25 21:20:02 -0300

    Adiciona diagnostico temporario de NODE_ENV

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ const verificarAutenticacao = require("./middlewares/authMiddleware");
 
 const app = express();
 
+console.log(
+  "NODE_ENV:",
+  process.env.NODE_ENV
+);
+
 app.set("trust proxy", 1);
 
 app.use(helmet());
